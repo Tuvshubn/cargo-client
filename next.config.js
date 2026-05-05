1d387cf7a0bac0f08c447bc9faef39f94c3c4d41
МонтоТрейд <montotrade@gmail.com>
2026-05-05 21:43:50 +0000

    fix: fully disable TS errors for build

--- next.config.js
+++ next.config.js
@@ -3,4 +3,5 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 module.exports = withNextIntl({
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  experimental: { optimizePackageImports: ['@mui/material', '@mui/icons-material'] },
 });
